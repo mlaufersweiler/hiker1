d4491538e591e92b06b9f48270ad16741eb66405
Michael Laufersweiler <michaellaufersweiler@Michaels-MacBook.local>
2019-08-15 21:36:59 -0500

    added deps and smsController

---
+++ server/controllers/smsController.js
@@ -0,0 +1,30 @@
+const MessagingResponse = require('twilio').twiml.MessagingResponse
+
+    module.exports = {
+        recieveSMS: async (req, res) => {
+            const twiml = new MessagingResponse();
+            const {Body, From} = req.body;
+            const textResponse = Body.replace(/[^a-zA-Z]/g, "").toUpperCase();
+            const hourExtension = Body.replace(/[^0-9]/g, "");
+            const userNumber = From.replace(/[^0-9]/g, "");
+            let db = req.app.get('db');
+            if(textResponse == 'BACK'){
+                await db.delete_alert_by_user_phone_number([userNumber])
+                twiml.message(`Great! We've canceled your alert. We hope you had a great trip.`);
+            } else if (textResponse == 'LATE'){
+                twiml.message(`Running late? No problem. Reply 'EXTEND' followed by the number of hours you'd like your anticipated return time to be postponed.`);
+            } else if (textResponse == 'EXTEND'){
+                await db.extend_alert([`${hourExtension}hour`, userNumber])
+                twiml.message(`We've extended your trip return time by ${hourExtension} hour(s). Happy hiking!`)
+            } else if (textResponse == 'SOS'){
+                await db.set_sos([userNumber])
+                twiml.message(`SOS received. We've alerted your designated contact that you're in trouble and emailed them your trip itinerary and personal information. Help is on the way soon.`)
+            } else {
+                twiml.message(
+                  `Sorry, we don't recognize that command. Please text 'BACK' if you've returned safely, 'LATE' if you're just running late, or 'SOS' if you need help.`
+                );
+            }
+                res.writeHead(200, {'Content-Type': 'text/xml'});
+                res.end(twiml.toString());
+              }
+    }
--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ const massive = require('massive');
 const session = require('express-session');
 
 const authController = require('./controllers/authController');
+const alertController = require('./controllers/alertController');
+
 
 const {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET} = process.env;
 
@@ -23,10 +25,20 @@ massive(CONNECTION_STRING).then(db => {
     console.log('database connected [:')
 })
 
+//auth endpoints
 app.post(`/auth/register`, authController.register)
 app.post(`/auth/login`, authController.login)
 app.get(`/auth/user-data`, authController.getUserData)
 app.get(`/auth/logout`, authController.logout)
+
+//alert endpoints
+app.post(`/api/createalert`, alertController.createAlert);
+app.get(`/api/alert-data/:id`, alertController.getAlertData);
+app.put(`/api/editalert`, alertController.editAlert);
+app.delete(`/api/deletealert/:id`, alertController.deleteAlert);
+
+//sms endpoints
+app.post('/sms',)
 
 app.listen(SERVER_PORT, () => {
     console.log(`Port ${SERVER_PORT} is open for business.`)
